1caf2eb902aed9638b1a6068962f463dcd58272f
fpuricelli <fpuricelli@gmail.com>
2022-10-15 19:37:53 -0300

    ejemplos AR y VR

---
+++ AR-1/Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 UnityEngine.GameObject UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::get_placedPrefab()
+extern void PlaceOnPlane_get_placedPrefab_m1BF21961559C84A37FA7D9DF8A8401B1BE83C416 (void);
+// 0x00000002 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::set_placedPrefab(UnityEngine.GameObject)
+extern void PlaceOnPlane_set_placedPrefab_m2B997DD7A3B3A637826C2F18DBECA44F97621657 (void);
+// 0x00000003 UnityEngine.GameObject UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::get_spawnedObject()
+extern void PlaceOnPlane_get_spawnedObject_mB3DAD65252D2D50E31C27D69BAC2F4BC7DEC18F6 (void);
+// 0x00000004 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::set_spawnedObject(UnityEngine.GameObject)
+extern void PlaceOnPlane_set_spawnedObject_m9369CCAEA1F5D013EF7B236C4B5A3F103373D356 (void);
+// 0x00000005 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::Awake()
+extern void PlaceOnPlane_Awake_m38433067061208AABB1F83B909A56AFCD20D150B (void);
+// 0x00000006 System.Boolean UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::TryGetTouchPosition(UnityEngine.Vector2&)
+extern void PlaceOnPlane_TryGetTouchPosition_m4B93C9635CCEAF3E5EB5CAB294AF08BE2BC8F9CC (void);
+// 0x00000007 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::Update()
+extern void PlaceOnPlane_Update_m9C8DC6FCAF258DBE231F1F09B53C410C7B3F46DD (void);
+// 0x00000008 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::.ctor()
+extern void PlaceOnPlane__ctor_m6DAE54BAA5E83A8A342A4C309093D1884D87C4C2 (void);
+// 0x00000009 System.Void UnityEngine.XR.ARFoundation.Samples.PlaceOnPlane::.cctor()
+extern void PlaceOnPlane__cctor_m58DB6162A7272070289E746E4C3F115DE5928695 (void);
+static Il2CppMethodPointer s_methodPointers[9] = 
+{
+	PlaceOnPlane_get_placedPrefab_m1BF21961559C84A37FA7D9DF8A8401B1BE83C416,
+	PlaceOnPlane_set_placedPrefab_m2B997DD7A3B3A637826C2F18DBECA44F97621657,
+	PlaceOnPlane_get_spawnedObject_mB3DAD65252D2D50E31C27D69BAC2F4BC7DEC18F6,
+	PlaceOnPlane_set_spawnedObject_m9369CCAEA1F5D013EF7B236C4B5A3F103373D356,
+	PlaceOnPlane_Awake_m38433067061208AABB1F83B909A56AFCD20D150B,
+	PlaceOnPlane_TryGetTouchPosition_m4B93C9635CCEAF3E5EB5CAB294AF08BE2BC8F9CC,
+	PlaceOnPlane_Update_m9C8DC6FCAF258DBE231F1F09B53C410C7B3F46DD,
+	PlaceOnPlane__ctor_m6DAE54BAA5E83A8A342A4C309093D1884D87C4C2,
+	PlaceOnPlane__cctor_m58DB6162A7272070289E746E4C3F115DE5928695,
+};
+static const int32_t s_InvokerIndices[9] = 
+{
+	2072,
+	1738,
+	2072,
+	1738,
+	2106,
+	1149,
+	2106,
+	2106,
+	3832,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	9,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
